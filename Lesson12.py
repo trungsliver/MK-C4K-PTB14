9f18d208e1cc87980fbb676ee526a26a47e7c3db
trungsliver <buiductrung.cva@gmail.com>
2024-08-18 09:34:05 +0700

    Lesson 12 upadte

--- Lesson12.py
+++ Lesson12.py
@@ -32,23 +32,65 @@ def abs_float(n:float):
 # 	YC1: Hàm nhận vào một danh sách các số nguyên.
 # 	YC2: Hàm trả về tổng các số lẻ trong danh sách đó.
 
+def sum_odd(numbers):
+    total = 0
+    for num in numbers:
+        if num % 2 != 0:  # Kiểm tra số lẻ
+            total += num
+    return total
+print(sum_odd([1, 2, 3, 4, 5]))  # Output: 9
+
 # Bài 2: Viết một hàm is_prime(n) để kiểm tra xem một số nguyên dương n có phải là số nguyên tố hay không.
 # 	YC1: Hàm nhận vào một số nguyên dương n.
 # 	YC2: Hàm trả về True nếu n là số nguyên tố, ngược lại trả về False.
 
+def is_prime(n):
+    for n in range(1,101):
+        count = 0
+        for i in range (1, n+1): 
+            if n % i == 0:
+                count += 1
+        if count == 2:
+            return True
+        else:
+            return False
+print(is_prime(7))  # Output: True
+print(is_prime(4))  # Output: False
+
 # Bài 3: Viết một hàm count_words(s) để đếm số lượng từ trong một chuỗi s.
 # 	YC1: Hàm nhận vào một chuỗi ký tự s.
 # 	YC2: Hàm trả về số lượng từ trong chuỗi đó.
+def count_words(s):
+    words = s.split()
+    return len(words)
+print(count_words("Hải meme đẹp trai"))  # Output: 4
 
 # Bài 4: Viết một hàm sum_of_digits(n) để tính tổng các chữ số của một số nguyên dương n.
 # 	YC1: Hàm nhận vào một số nguyên dương n.
 # 	YC2: Hàm trả về tổng các chữ số của n.
+def sum_of_digits(n):
+    total = 0
+    for digit in str(n):
+        total += int(digit)
+    return total
+print(sum_of_digits(12345))  # Output: 15
 
 # Bài 5: Viết một hàm find_max(numbers) để tìm vị trí số lớn nhất trong một danh sách numbers.
 # 	YC1: Hàm nhận vào một danh sách các số nguyên.
 # 	YC2: Hàm trả về vị trí số lớn nhất trong danh sách đó.
+def find_max(numbers):
+    max_value = max(numbers)
+    for i in range(len(numbers)):
+        if numbers[i] == max_value:
+            return i
+print(find_max([1, 2, 3, 2, 1]))  # Output: 2
 
 # Bài 6: Viết một hàm sum_to_n(n) để tính tổng các số từ 1 đến n.
 # 	YC1: Hàm nhận vào một số nguyên dương n.
 # 	Yc2: Hàm trả về tổng các số từ 1 đến n.
-
+def sum_to_n(n):
+    sum = 0
+    for i in range(n+1):
+        sum = sum + i
+    return sum
+print(sum_to_n(5))  # Output: 15
